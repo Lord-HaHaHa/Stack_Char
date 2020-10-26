990a58a3e462f889e61950e01ad529bceabbd158
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2020-10-26 08:48:56 +0100

    dies ist ein sinnvoller commit

--- Stack_Char/MainWindow.xaml.cs
+++ Stack_Char/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,39 @@ namespace Stack_Char
     /// </summary>
     public partial class MainWindow : Window
     {
+        Stack stack = new Stack(3);
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_Push_Click(object sender, RoutedEventArgs e)
+        {
+            stack.Push();
+        }
+
+        private void btn_Pop_Click(object sender, RoutedEventArgs e)
+        {
+            stack.Pop();
+        }
+        public class Stack
+        {
+
+            char[] elemente;
+
+            public Stack(int length)
+            {
+                elemente = new char[length];
+            }
+
+            public bool Push()
+            {
+                return true;
+            }
+            public string Pop()
+            {
+                return  "";
+            }
         }
     }
 }
